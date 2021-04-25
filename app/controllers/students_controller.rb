437ce033c725a440b2ede775f75ca96a4bd40991
Michael Nnaji <mnnaji3@uwo.ca>
2021-04-25 07:18:08 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+ def new
+ end
+
+ def create 
+   @student = Student(first_name: params[:first_name],last_name: params[:last_name])
+   params.inspect
+ end
+ 
 end
